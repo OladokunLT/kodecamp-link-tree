4724d5c83e755371d9c77faf33a494a442623691
oladokunlt <lukmanoladokun86@gmail.com>
2023-08-12 19:28:30 +0100

    Finish header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import profile  from './assets/profile-img.png'
+import smShare  from './assets/share-icon-arrow.svg'
+import mdShare  from './assets/share-icon-desktop.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrapper">
+        <header className="header">
+          <figure>
+            <div className='profile-container'>
+             <img src={profile} alt="User profile image" />
+            </div>
+            <figcaption className="username">John Doe</figcaption>
+          </figure>
+        </header>
+        <main className="main-section">
+          
+        </main>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
